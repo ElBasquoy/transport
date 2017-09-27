9fe9d34c3a6241b8aa1af1779a580db1159764fd
Sébastien GONZALEZ <sebastien@matsiya.com>
2017-09-27 11:31:11 +0200

    modif

--- docs/main.js
+++ docs/main.js
@@ -6,31 +6,38 @@
 	return false;
 });*/
 
-$(".body-slogan").css({ minHeight: ($(window).innerHeight()- 70) + 'px' });
+$(".body-slogan").css({ minHeight: ($(window).innerHeight()) + 'px' });
 $(".body-histoy").css({ minHeight: ($(window).innerHeight()) + 'px' });
 $(".body-car").css({ minHeight: ($(window).innerHeight()) + 'px' });
 $(".body-car").css({ minHeight: ($(window).innerHeight()) + 'px' });
 $(".body-flotte").css({ minHeight: ($(window).innerHeight()) + 'px' });
-$(".body-ramassage").css({ minHeight: ($(window).innerHeight()) + 'px' });
 $(".body-regu").css({ minHeight: ($(window).innerHeight()) + 'px' });
 
 $( window ).resize(function() {
-  $(".body-slogan").css({ minHeight: ($(window).innerHeight()- 70) + 'px' });
+  $(".body-slogan").css({ minHeight: ($(window).innerHeight()) + 'px' });
   $(".body-histoy").css({ minHeight: ($(window).innerHeight()) + 'px' });
   $(".body-car").css({ minHeight: ($(window).innerHeight()) + 'px' });
   $(".body-car").css({ minHeight: ($(window).innerHeight()) + 'px' });
   $(".body-flotte").css({ minHeight: ($(window).innerHeight()) + 'px' });
-  $(".body-ramassage").css({ minHeight: ($(window).innerHeight()) + 'px' });
   $(".body-regu").css({ minHeight: ($(window).innerHeight()) + 'px' });
 });
+console.log('fzef')
+console.log('fzef')
+console.log('fzef')
 
 $("#activity").click(function() {
+  console.log('fzefzefez')
 	$("html, body").animate({
 		scrollTop: $(".body-activity").offset().top
 	}, "slow");
 	return false;
 });
+$('#activity').click(function() {
+  alert('Handler for .click() called.');
+});
+
 $("#history").click(function() {
+  console.log('fzef')
 	$("html, body").animate({
 		scrollTop: $(".body-history").offset().top
 	}, "slow");
@@ -57,6 +64,18 @@ $("#ramassage").click(function() {
 $("#regu").click(function() {
 	$("html, body").animate({
 		scrollTop: $(".body-regu").offset().top
+	}, "slow");
+	return false;
+});
+$("#contact").click(function() {
+	$("html, body").animate({
+		scrollTop: $(".body-contact").offset().top
+	}, "slow");
+	return false;
+});
+$("#slogan").click(function() {
+	$("html, body").animate({
+		scrollTop: $(".body-slogan").offset().top
 	}, "slow");
 	return false;
 });
